985c5be66086240ae440f56c703354e800f5d464
lzkgmr <lzkgmr@gmail.com>
2025-12-21 17:50:41 +0300

    added launchscreen

--- karto4ki/karto4ki/ViewController.swift
+++ karto4ki/karto4ki/ViewController.swift
@@ -11,6 +11,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let background = UIImage(named: "background")
+        let backgroundView = UIImageView(image: background)
+        backgroundView.contentMode = .scaleToFill
+        
+        view.addSubview(backgroundView)
+        backgroundView.pinTop(to: view.topAnchor)
+        backgroundView.pinLeft(to: view.leadingAnchor)
+        backgroundView.pinBottom(to: view.bottomAnchor)
+        backgroundView.pinRight(to: view.trailingAnchor)
+        
+
+        for family in UIFont.familyNames.sorted() {
+            print("Family: \(family)")
+            for name in UIFont.fontNames(forFamilyName: family) {
+                print("   \(name)")
+            }
+        }
+
         // Do any additional setup after loading the view.
     }
 
