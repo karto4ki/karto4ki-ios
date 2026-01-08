6923d0dab5aed49e6ae85b6574cedc606eeb030f
lzkgmr <lzkgmr@gmail.com>
2026-01-08 20:12:46 +0300

    added empty registration

---
+++ karto4ki/karto4ki/Screens/Registration/RegistrationAssembly.swift
@@ -0,0 +1,15 @@
+//
+//  RegistrationAssembly.swift
+//  karto4ki
+//
+//  Created by лизо4ка курунок on 08.01.2026.
+//
+
+import Foundation
+
+struct RegistrationAssembly {
+    static func build() -> RegistrationViewController {
+        let view = RegistrationViewController()
+        return view
+    }
+}
