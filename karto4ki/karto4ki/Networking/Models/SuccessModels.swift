645428a89649d9772f8edeebe058b5933162fa8b
lzkgmr <lzkgmr@gmail.com>
2026-02-11 16:29:33 +0300

    added token manager without keychain

--- karto4ki/karto4ki/Networking/Models/SuccessModels.swift
+++ karto4ki/karto4ki/Networking/Models/SuccessModels.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+enum SuccessModels {
+    
+    struct Tokens: Codable {
+        let accessToken: String
+        let refreshToken: String
+        
+        enum CodingKeys: String, CodingKey {
+            case accessToken = "access_token"
+            case refreshToken = "refresh_token"
+        }
+    }
+}
---
+++ karto4ki/karto4ki/Services/IdentityService/IdentityService.swift
@@ -0,0 +1,8 @@
+//
+//  IdentityService.swift
+//  karto4ki
+//
+//  Created by лизо4ка курунок on 11.02.2026.
+//
+
+import Foundation
